01a06fa6c77be755ba4f6548f372e46d2f8eb8f4
pacificpheonix <60445756+pacificpheonix@users.noreply.github.com>
2020-03-22 17:19:49 -0600

    Update Name.java

--- src/Name.java
+++ src/Name.java
@@ -4,5 +4,6 @@ public class Name {
     public static void main(String[] args){
 
         System.out.println("Hello, my name is Liya Wang.");
+        System.out.println("Hello Liya, I'm Reece!");
     }
 }
